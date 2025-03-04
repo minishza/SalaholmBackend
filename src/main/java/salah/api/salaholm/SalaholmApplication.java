36e49efdc8dfc75fa16f34c39c5f1c05f8c8fd90
MAIN <muhammad.shamaeem@protonmail.com>
2025-03-04 12:39:55 +0100

    initiate project

---
+++ src/main/java/salah/api/salaholm/SalaholmApplication.java
@@ -0,0 +1,13 @@
+package salah.api.salaholm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaholmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaholmApplication.class, args);
+    }
+
+}
