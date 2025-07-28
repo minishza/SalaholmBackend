da9d00b97dd2c1dbbcddd7b8768ecb98e338d7da
Muhammad S <106777360+minishza@users.noreply.github.com>
2025-07-28 18:27:36 +0200

    implement empty methods of prayerservice interface

--- src/main/java/salah/api/salaholm/service/PrayerService.java
+++ src/main/java/salah/api/salaholm/service/PrayerService.java
@@ -2,12 +2,74 @@ package salah.api.salaholm.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import salah.api.salaholm.dto.location.LocationDTO;
+import salah.api.salaholm.dto.prayer.PrayersDTO;
 import salah.api.salaholm.repository.LocationPrayerRepository;
 import salah.api.salaholm.util.scraper.PrayerScraper;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class PrayerService {
+public class PrayerService implements PrayerServiceInterface {
     private final LocationPrayerRepository locationPrayerRepository;
     private final PrayerScraper prayerScraper;
+
+    @Override
+    public Optional<LocationDTO> getLocationDTO(String city) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<PrayersDTO> getPrayersByCity(String city) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<PrayersDTO> getPrayersByDate() {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<PrayersDTO> getMonthlyPrayers(String city, YearMonth month) {
+        return List.of();
+    }
+
+    @Override
+    public List<PrayersDTO> getAllAnnualPrayers(String city) {
+        return List.of();
+    }
+
+    @Override
+    public List<String> getSupportedCities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean locationExists(String city) {
+        return false;
+    }
+
+    @Override
+    public String getMunicipality(String city) {
+        return "";
+    }
+
+    @Override
+    public double[] getCoordinates(String city) {
+        return new double[0];
+    }
+
+    @Override
+    public String getFormattedLocationName(String city) {
+        return "";
+    }
+
+    @Override
+    public List<LocalDate> getAvailablePrayerDates(String city) {
+        return List.of();
+    }
 }
